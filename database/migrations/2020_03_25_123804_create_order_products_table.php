9ed27bb096a396ff247611c6bd1bd299f7c5578e
Muhammad Zaid Al Anshori <zaid.al.anshori@gmail.com>
2020-03-25 20:00:51 +0700

    Add Migration Table

--- database/migrations/2020_03_25_123804_create_order_products_table.php
+++ database/migrations/2020_03_25_123804_create_order_products_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateOrderProductsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('order_products', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->integer('role');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->bigInteger('orderId');
+            $table->bigInteger('productId');
+            $table->integer('quantity');
+            $table->bigInteger('price');
+            $table->bigInteger('totalPrice');
             $table->timestamps();
             $table->softDeletes();
         });
@@ -33,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('order_products');
     }
 }
